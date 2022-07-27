1ad90302b1f95cf992bcd47be2e1b0d8ef7efea1
Taekwan Park <techigh@Taekwans-MacBook-Pro.local>
2022-07-27 22:49:28 +0900

    nav

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav>
-      <Link to="/invoices">Invoices</Link>
+    <nav className="flex gap-x-3 justify-center bg-red-300">
+      <Link to="/">
+        <div className="rounded-md p-2 shadow-md backdrop-opacity-10">Home</div>
+      </Link>
+      <Link to="/works">Works</Link>
       <Link to="/components">Components</Link>
       <Link to="/about">About</Link>
     </nav>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
-import Button from "@components/element/Button";
 import Layout from "@components/layout/Layout";
 
 const Home = () => {
   return (
     <Layout pageTitle="Home">
       <div>Home</div>
-      <Button text="button"></Button>
     </Layout>
   );
 };
