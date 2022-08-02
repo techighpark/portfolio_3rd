b1deb9d92e19a5f88fdcd47e21289b40044ac874
Taekwan Park <techigh@Taekwans-MacBook-Pro.local>
2022-08-02 20:42:39 +0900

    tailwind custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,40 +2,41 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: "black",
-      white: "white",
-      primary: {
-        light: "#ff6c4d",
-        default: "#ff421a",
-        dark: "#e62900",
-      },
-      secondary: {
-        light: "#ff785b",
-        default: "#FF5733",
-        dark: "#ff785b",
-      },
-      accent: {
-        light: "#ff785b",
-        default: "#FF5733",
-        dark: "#ff785b",
-      },
-      success: "",
-      warning: "",
-      danger: "",
-      basic: {
-        dark: {
-          light: "#010101",
-          default: "#1b1b1b",
-          dark: "#343434",
+    extend: {
+      colors: {
+        // transparent: "transparent",
+        // current: "currentColor",
+        // black: "black",
+        // white: "white",
+        primary: {
+          light: "#ff6c4d",
+          DEFAULT: "#ff421a",
+          dark: "#e62900",
         },
-        lgiht: {
-          light: "#ffffff",
-          default: "#f2f2f2",
-          dark: "#d8d8d8",
+        secondary: {
+          light: "#ff785b",
+          DEFAULT: "#FF5733",
+          dark: "#ff785b",
+        },
+        accent: {
+          light: "#ff785b",
+          DEFAULT: "#FF5733",
+          dark: "#ff785b",
+        },
+        success: "",
+        warning: "",
+        danger: "",
+        basic: {
+          dark: {
+            light: "#010101",
+            DEFAULT: "#1b1b1b",
+            dark: "#343434",
+          },
+          lgiht: {
+            light: "#ffffff",
+            DEFAULT: "#f2f2f2",
+            dark: "#d8d8d8",
+          },
         },
       },
     },
