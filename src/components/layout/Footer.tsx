67e531b2b56a59ecb53691876474ad7dac947353
Taekwan Park <techigh.b@gmail.com>
2022-08-07 18:03:59 +0900

    footer&nav

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,6 @@ import Nav from "../element/Nav";
 const Footer = (props: any, ref: any) => {
   return (
     <div className="absolute inset-x-0 bottom-10 flex h-24 items-center justify-center">
-      Footer
       <Nav ref={ref} />
     </div>
   );
--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,30 +1,12 @@
 import LinkBtn from "@components/element/LinkBtn";
 
 const Nav = () => {
-  // const { pathname } = useLocation();
-
   return (
     <nav className="flex justify-center space-x-5 rounded-xl border bg-white px-4 pb-3 pt-2 shadow-inner drop-shadow-lg backdrop-blur-sm backdrop-opacity-30">
-      <LinkBtn
-        to="/"
-        text="Main"
-        // active={pathname === "/" ? true : false}
-      />
-      <LinkBtn
-        to="/about"
-        text="About"
-        // active={pathname === "/about" ? true : false}
-      />
-      <LinkBtn
-        to="/works"
-        text="Works"
-        // active={pathname === "/works" ? true : false}
-      />
-      <LinkBtn
-        to="/components"
-        text="Comp"
-        // active={pathname === "/components" ? true : false}
-      />
+      <LinkBtn to="/" text="Main" />
+      <LinkBtn to="/about" text="About" />
+      <LinkBtn to="/works" text="Works" />
+      <LinkBtn to="/components" text="Comp" />
     </nav>
   );
 };
